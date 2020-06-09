6eb4157a1e4230426c2ca2c13f9022ea2746c54d
song <1115064450@qq.com>
2020-06-10 00:43:28 +0800

    修改配置文件类型为yaml

--- wegrow-security/src/main/java/com/wegrow/wegrow/security/config/IgnoreUrlsConfig.java
+++ wegrow-security/src/main/java/com/wegrow/wegrow/security/config/IgnoreUrlsConfig.java
@@ -3,8 +3,6 @@ package com.wegrow.wegrow.security.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -14,7 +12,7 @@ import java.util.List;
 @Getter
 @Setter
 @Component
-@ConfigurationProperties(prefix = "security.ignored")
+@ConfigurationProperties(prefix = "security.ignored") // 读取配置文件中的默认忽略url
 public class IgnoreUrlsConfig {
     private List<String> urls = new ArrayList<>();
 }
