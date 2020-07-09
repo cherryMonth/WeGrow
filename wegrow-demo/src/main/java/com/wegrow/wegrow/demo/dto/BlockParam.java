210169a697c1114b6cc081d2d2eb96ace3616a5a
song <1115064450@qq.com>
2020-07-10 01:08:25 +0800

    完成前端的Block创建、修改与预览功能，支持保存(ctrl + s)存储到草稿箱。

--- wegrow-demo/src/main/java/com/wegrow/wegrow/demo/dto/BlockParam.java
+++ wegrow-demo/src/main/java/com/wegrow/wegrow/demo/dto/BlockParam.java
@@ -4,9 +4,6 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-
 
 @Getter
 @Setter
@@ -15,7 +12,6 @@ public class BlockParam {
     private String title;
 
     @ApiModelProperty(value = "所属的专题ID")
-    @NotNull(message = "专题不能为空")
     private Integer topicId;
 
     @ApiModelProperty(value = "内容的初始状态")
