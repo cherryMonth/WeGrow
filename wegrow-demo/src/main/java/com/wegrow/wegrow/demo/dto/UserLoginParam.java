fd58fc92b465c317c0f4967feb6364567ba7a008
song <1115064450@qq.com>
2020-08-16 16:27:07 +0800

    修复不提交用户头像也可以创建成功的bug

--- wegrow-demo/src/main/java/com/wegrow/wegrow/demo/dto/UserLoginParam.java
+++ wegrow-demo/src/main/java/com/wegrow/wegrow/demo/dto/UserLoginParam.java
@@ -4,12 +4,17 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 public class UserLoginParam {
     @ApiModelProperty(value = "用户名", required = true)
+    @NotBlank
     private String username;
 
     @ApiModelProperty(value = "密码", required = true)
+    @NotEmpty
     private String password;
 }
--- wegrow-demo/src/main/java/com/wegrow/wegrow/demo/dto/UserParam.java
+++ wegrow-demo/src/main/java/com/wegrow/wegrow/demo/dto/UserParam.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
@@ -16,20 +17,23 @@ import javax.validation.constraints.Size;
 public class UserParam {
 
     @ApiModelProperty(value = "用户邮箱", required = true)
+    @NotBlank
     @Email(message = "邮箱格式不合法")
     private String email;
 
     @ApiModelProperty(value = "用户昵称", required = true)
-    @NotEmpty(message = "用户昵称不能为空")
+    @NotBlank(message = "用户昵称不能为空")
     private String username;
 
     @ApiModelProperty(value = "用户简介")
     private String aboutMe;
 
     @ApiModelProperty(value = "用户头像HASH", required = true)
+    @NotBlank
     private String avatarHash;
 
     @ApiModelProperty(value = "密码", required = true)
+    @NotEmpty
     @Size(min=6, max = 20, message = "密码长度应该在6和20之间")
     private String password;
 }
